e85eee753bd8e69184af6284f45b1a29ef1e5717
Firuz333 <89309260+Firuz333@users.noreply.github.com>
2021-08-27 19:54:16 +0500

    I created the a project

---
+++ MvcProject/Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+namespace MvcProject.Controllers
+{
+    public class StudentController:Controller
+    {
+        public string Hello()
+        {
+            return "Hello world";
+
+        }
+        
+                
+    }
+}
